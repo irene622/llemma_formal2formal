bd6075e331228834b60d035192c6c6be67f2709c
irene622 <dywjd7272@naver.com>
2024-09-07 18:56:27 +0900

    add logs argument in shell

--- scripts/eval_llemma7b.sh
+++ scripts/eval_llemma7b.sh
@@ -5,6 +5,7 @@ TIMEOUT=600
 NUM_SHARDS=2
 DATASET="minif2f-test"
 DATA="data/minif2f.jsonl"
+NUMBER="05"
 
 MODEL="open-web-math/llemma_7b"
 NAME="llemma7b"
@@ -13,6 +14,6 @@ OUTPUT_DIR="output/${NAME}_minif2f_test"
 
 for SHARD in 2 3
 do
-  CUDA_VISIBLE_DEVICES=${SHARD} python proofsearch.py --dataset-name ${DATASET} --temperatures ${TEMPERATURES} --timeout ${TIMEOUT} --num-shards ${NUM_SHARDS} --shard ${SHARD} --model-name ${MODEL} --max-iters ${MAX_ITERS} --dataset-path ${DATA} --num-samples ${NUM_SAMPLES} --early-stop --output-dir ${OUTPUT_DIR} &> ${NAME}_shard${SHARD}_03.out &
+  CUDA_VISIBLE_DEVICES=${SHARD} python proofsearch.py --dataset-name ${DATASET} --temperatures ${TEMPERATURES} --timeout ${TIMEOUT} --num-shards ${NUM_SHARDS} --shard ${SHARD} --model-name ${MODEL} --max-iters ${MAX_ITERS} --dataset-path ${DATA} --num-samples ${NUM_SAMPLES} --early-stop --output-dir ${OUTPUT_DIR} &>> ${NAME}_shard${SHARD}_${NUMBER}.txt 2>&1
 done
 
